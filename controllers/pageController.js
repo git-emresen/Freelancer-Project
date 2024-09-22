007119593141449aafa28736068e373246bdfc81
Your Name <you@example.com>
2024-09-23 00:40:13 +0300

    the project has finished

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,8 +1,15 @@
+const Portfolio=require("../models/portfolioModel")
 
 exports.getIndexPage=async (req,res)=>{
+    //await yalmadığı için çalışmaz
+    /* const portfolios=Portfolio.find()
     res.render("index",{
-        null:null
-     })
+        portfolios        
+     }) */      
+        const portfolios= await Portfolio.find().sort('-createdAt')
+        res.render("index",{
+           portfolios
+        })
 }
 
 exports.getAdminPage=async (req,res)=>{
